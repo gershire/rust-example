7bf3bb23ad36a7e4979f038c9f1531eaf2d5abab
Jacob Grachevsky <jacobgrach@gmail.com>
2021-10-07 10:58:37 +0300

    Create database

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::io;
-use std::str::Split;
 use rocksdb::{DB, Options};
 
 fn main() -> io::Result<()> {
+    let path = "./data";
+    let db = DB::open_default(path).unwrap();
     help();
     let mut buffer = String::new();
     loop {
@@ -13,20 +14,23 @@ fn main() -> io::Result<()> {
             match *command {
                 "help" => help(),
                 "quit" => break,
-                "put" => handle_put(&split),
-                "get" => handle_get(&split),
-                "delete" => handle_delete(&split),
+                "put" => handle_put(&split, &db),
+                "get" => handle_get(&split, &db),
+                "delete" => handle_delete(&split, &db),
                 _ => println!("Unknown command: {}", buffer)
             }
         }
     }
+    // Uncomment the next line to destroy the database on exit
+    // let _ = DB::destroy(&Options::default(), path);
     Ok(())
 }
 
-fn handle_put(cmd: &Vec<&str>) {
+fn handle_put(cmd: &Vec<&str>, db: &rocksdb::DB) {
     if let Some(key) = cmd.get(1) {
         if let Some(value) = cmd.get(2) {
             println!("ADDING RECORD: {} -> {}", *key, *value);
+            db.put(*key, *value).unwrap();
         } else {
             println!("Value not specified")
         }
@@ -35,17 +39,23 @@ fn handle_put(cmd: &Vec<&str>) {
     }
 }
 
-fn handle_get(cmd: &Vec<&str>) {
+fn handle_get(cmd: &Vec<&str>, db: &rocksdb::DB) {
     if let Some(key) = cmd.get(1) {
         println!("GETTING RECORD: {}", *key);
+        match db.get(*key) {
+            Ok(Some(value)) => println!("the value is: {}", String::from_utf8(value).unwrap()),
+            Ok(None) => println!("value not found"),
+            Err(e) => println!("error: {}", e)
+        }
     } else {
         println!("Key not specified")
     }
 }
 
-fn handle_delete(cmd: &Vec<&str>) {
+fn handle_delete(cmd: &Vec<&str>, db: &rocksdb::DB) {
     if let Some(key) = cmd.get(1) {
         println!("DELETING RECORD: {}", *key);
+        db.delete(*key).unwrap();
     } else {
         println!("Key not specified")
     }
